294d46c21fe72ff9f71ea78e81c68b4da3147aea
Juan <juan.hernando@netcentric.biz>
2022-10-20 12:45:04 +0200

    Issue 8. Remove marquee.

--- script/app.js
+++ script/app.js
@@ -1,4 +1,3 @@
-const CONTENT_BREAKING_NEWS = "BREAKING NEWS: 5 people arrested for non designing responsively ***** Sprite popularity drops even further after new, less sugary, drink introduction.*****"
 const CONTENT_ARTICLE_TEASERS = [
   "This is an article about first party that happened this weekend",
   "This is an article about second party that happened this weekend",
@@ -24,10 +23,6 @@ const lazyLoad = () => {
 
 const dynamicContent = () => {
   const teasers = document.querySelectorAll('.teaser__text');
-  const marqueeBar = document.querySelector('marquee');
-
-  // mock content delay
-  setTimeout(() => marqueeBar.innerHTML = CONTENT_BREAKING_NEWS, 3000);
 
   // create dynamic content
   teasers.forEach((teaser, index) => {
